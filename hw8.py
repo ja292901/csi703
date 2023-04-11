78a9abc86153265ef8f5f4e142b6edf50939c2e9
ja292901 <89234619+ja292901@users.noreply.github.com>
2023-04-10 22:12:20 -0400

    Add files via upload

--- hw8.py
+++ hw8.py
@@ -3,10 +3,10 @@
 Created on Mon Apr 10 21:27:33 2023
 
 @author: ja292
+based on https://github.com/GMU-instructor/Teaching_public/streamlit-demo.py
 """
 
-#Import libraries
-#from re import X
+
 import streamlit as st
 import seaborn as sns
 import matplotlib
@@ -18,26 +18,16 @@ from plotly.subplots import make_subplots
 import plotly.express as px
 
 #Load and clean data
-data = sns.load_dataset('penguins')
-data['bill_length_mm'].fillna(data['bill_length_mm'].mean(), inplace=True)
-data['bill_depth_mm'].fillna(data['bill_depth_mm'].mean(), inplace=True)
-data['flipper_length_mm'].fillna(data['flipper_length_mm'].mean(), inplace=True)
-data['body_mass_g'].fillna(data['body_mass_g'].mean(), inplace=True)
-data['sex'].fillna(data['sex'].value_counts().index[0], inplace=True)
+data = sns.load_dataset('mpg')
+data = data.dropna(axis='rows')      # drop rows with empty data
 
 
 #Creating more user-friendly option labels
-data.rename(columns = 
-    {"flipper_length_mm": "flipper length",  
-    "bill_length_mm":"bill length",
-    "bill_depth_mm":"bill depth",
-    "body_mass_g":"body mass"},
-    inplace = True)
-featureset = ['flipper length', 'bill length', 'bill depth', 'body mass', 'island', 'sex', 'species']
+featureset = list(data.columns.values)
 graphset = ["Histogram", "Box Plot", "Enhanced Box Plot", "Strip Plot", "Violin Plot", "Swarm Plot"]
 
 #Give our dashboard a title
-st.title('Customer dashboard')
+st.title('Vehicle dashboard')
 
 #Show the raw data
 st.subheader('Raw data')
@@ -48,7 +38,7 @@ st.subheader('Visualization')
 col1, col2 = st.columns(2)
 with col1:
     st.subheader('Choices')
-    title = st.text_input('Enter a title for the chart')
+    title = st.text_input('Chart Title')
     kind1 = st.selectbox('Pick plot for left side', graphset, key="pl")
     x = st.selectbox("Feature for x:", featureset, key="f1l")
     if kind1 != "Histogram":
